d58dd8b454e36191381119d51bd6b7ff8fb9ed51
KornelKopanski <kkornel1771@gmail.com>
2019-09-05 20:08:54 +0200

    Poprawiono nasz model

--- project/src/psychodjango/products/models.py
+++ project/src/psychodjango/products/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
 
-    name = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
+    name = models.CharField(max_length=120) #maksymalna ilość znaków
+    description = models.TextField(blank=True,null=True) # w tym momencie wpisywanie opisu nie jest konieczne
+    price = models.DecimalField(max_digits=10000,decimal_places=2) # max_digits - maksymalna cena, decimal_places - miejsca po przecinku
     summary = models.TextField(default= 'Django jest ok')
 
