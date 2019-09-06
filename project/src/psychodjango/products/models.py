de5309b33c65b545332e99da9393f1d00af5b11b
KornelKopanski <kkornel1771@gmail.com>
2019-09-06 18:40:49 +0200

    Modyfikacja modelu

--- project/src/psychodjango/products/models.py
+++ project/src/psychodjango/products/models.py
@@ -7,4 +7,6 @@ class Product(models.Model):
     description = models.TextField(blank=True,null=True) # w tym momencie wpisywanie opisu nie jest konieczne
     price = models.DecimalField(max_digits=10000,decimal_places=2) # max_digits - maksymalna cena, decimal_places - miejsca po przecinku
     summary = models.TextField(default= 'Django jest ok')
+    discount = models.BooleanField()
+
 
